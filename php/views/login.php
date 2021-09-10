33da00298335eb4446459c5e05467f25d07ec118
BastienZANARDO <bastien@fzanardo.fr>
2021-09-10 10:52:00 +0200

    1er test login

--- php/views/login.php
+++ php/views/login.php
@@ -1,5 +1,23 @@
 <?php
-include('./layouts/structure.php');
-?>
+$title = 'Login';
 
-<button type="button" class="btn btn-danger">Danger</button>
+$content ='
+<form method="POST" action="./index.php?action=login>
+  <fieldset>
+    <legend>Merci de vous connecter.</legend>
+
+    <div class="form-group">
+      <label for="exampleInputEmail1" class="form-label mt-4">Adresse Mail</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Rentrer votre adresse mail" name="email">
+      
+    </div>
+
+    <div class="form-group">
+      <label for="exampleInputPassword1" class="form-label mt-4">Mot de Passe</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de Passe" name="password">
+    </div>
+    <br>
+    <button type="submit" class="btn btn-primary">Envoyer</button>
+</form>
+';
+require_once('./layouts/structure.php');
