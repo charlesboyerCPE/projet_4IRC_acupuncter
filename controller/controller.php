c4b2ce78e5f91b437d1e4ad35a47e8547de41058
Lornage Rémy <37376697+FireRoumy@users.noreply.github.com>
2021-09-15 04:56:46 +0200

    controller index.php

--- controller/controller.php
+++ controller/controller.php
@@ -12,42 +12,45 @@ function getlogin(){
 }
 
 function postlogin(){
-
   $smarty = new Smarty;
   $smarty->display('index.php');
 }
 
-/* function getlogin(){
+function register(){
   $smarty = new Smarty;
-  $smarty->display('./views/login.html');
+  $smarty->display('./views/register.html');
 }
 
-function postlogin(){
-  
+function information(){
   $smarty = new Smarty;
-  $smarty->display('index.php');
-} */
+  $smarty->display('./views/index.html');
+}
+
+function bienfaits(){
+  $smarty = new Smarty;
+  $smarty->display('./views/index.html#Les bienfaits');
+}
+
+function location(){
+  $smarty = new Smarty;
+  $smarty->display('./views/index.html#Nous trouver');
+}
+
+
 
 /*   error_reporting(E_ALL);
   ini_set("display_errors", 1);
-
   // function index() {
   include('../models/acupuncter.php');
-
   $acu = new Acupuncter();
-
   $acu->connect();
   $acu->obtenirPatho();
-
   // RÃ©cuperation de la liste des pathologies
   $sql = "SELECT * FROM patho";
   $query = $this->db->prepare($sql);
   $query->execute();
-
   $listePatho = $query->fetchAll();
-
   $this->loadView('../views/home.php', $listePatho);
   // include('../views/home.php');
   // }
-
 ?> */
