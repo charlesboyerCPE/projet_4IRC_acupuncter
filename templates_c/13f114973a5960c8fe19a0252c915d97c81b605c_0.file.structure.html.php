20fc57b0e924cdcacac3ea4003e34606689c4631
lucasthorelcpe <lucas.thorel@cpe.fr>
2021-09-30 16:23:06 +0200

    new

--- templates_c/13f114973a5960c8fe19a0252c915d97c81b605c_0.file.structure.html.php
+++ templates_c/13f114973a5960c8fe19a0252c915d97c81b605c_0.file.structure.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-09-30 11:35:44
+/* Smarty version 3.1.39, created on 2021-09-30 14:21:26
   from 'C:\wamp64\www\projet_4IRC_acupuncter\templates\structure.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6155a110aa2154_42176393',
+  'unifunc' => 'content_6155c7e65274f0_01056379',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '13f114973a5960c8fe19a0252c915d97c81b605c' => 
     array (
       0 => 'C:\\wamp64\\www\\projet_4IRC_acupuncter\\templates\\structure.html',
-      1 => 1632744706,
+      1 => 1633011582,
       2 => 'file',
     ),
   ),
@@ -21,21 +21,21 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:./nav.html' => 1,
   ),
 ),false)) {
-function content_6155a110aa2154_42176393 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6155c7e65274f0_01056379 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="fr">
 <head>
   <meta charset="utf-8">
   <title>Acuponcture | <?php echo $_smarty_tpl->tpl_vars['title']->value;?>
 </title>
-  <link rel="stylesheet" href="css/main.css">
   <link rel="stylesheet" href="css/bootstrap.css">
   <link rel="stylesheet" href="css/index.css">
+  <?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"><?php echo '</script'; ?>
+>
 </head>
 <body>
   <?php $_smarty_tpl->_subTemplateRender("file:./nav.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
-  <div id="Accueil" class="container">
-
-<?php }
+  <div class="container body-filter"><?php }
 }
--- templates_c/5b0560685ec9285332b3f7e3da6c91a7950114ac_0.file.index.html.php
+++ templates_c/5b0560685ec9285332b3f7e3da6c91a7950114ac_0.file.index.html.php
@@ -1,101 +1,104 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-09-30 11:35:44
+/* Smarty version 3.1.39, created on 2021-09-30 14:12:21
   from 'C:\wamp64\www\projet_4IRC_acupuncter\views\index.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6155a1109cda38_72873908',
+  'unifunc' => 'content_6155c5c5258a77_00454452',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5b0560685ec9285332b3f7e3da6c91a7950114ac' => 
     array (
       0 => 'C:\\wamp64\\www\\projet_4IRC_acupuncter\\views\\index.html',
-      1 => 1632754076,
+      1 => 1633011049,
       2 => 'file',
     ),
   ),
   'includes' => 
   array (
     'file:../templates/structure.html' => 1,
+    'file:../templates/structure-f.html' => 1,
   ),
 ),false)) {
-function content_6155a1109cda38_72873908 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6155c5c5258a77_00454452 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:../templates/structure.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array('title'=>"Accueil"), 0, false);
 ?>
 
 <section>
-    <div class="row align-items-start">
-        <div class="col">
-            <br><br>
-            <h1 class="text-center">Acueil : Acupunture CPE LYON</h1>
-            <br>
+    <div class="row">
+        <div class="col-sm my-4 text-center">
+            <h1>Acupunture CPE Lyon</h1>
         </div>
     </div>
 </section>
 
 <section id="Information">
     <!-- inforamtion sur Acupunture fonctionnement + image aiguille... section divisé en deux horizontalement -->
-    <div class="container">
-        <div class=row>
-            <div class="description col-sm">
-                <h1 class=Page>Qu'est-ce que l'acuponcture ?</h1>
-                <p>L’acupuncture ou acuponcture est un système thérapeutique dont les origines historiques sont
-                    très liées avec la tradition médicale chinoise. L'acupuncture consiste en une stimulation de zones
-                    précises de l'épiderme : les « points d’acupuncture ». Les techniques de stimulation des points
-                    d’acupuncture sont effectuées avec des moyens divers : des aiguilles le plus souvent, mais aussi
-                    d'autres moyens physiques (mécaniques, électriques, magnétiques, thermiques, lumineux) ou
-                    physico-chimiques, voire d'autres méthodes alternatives dangereuses (apipuncture, apithérapie avec
-                    des piqûres d'abeille).
-                    L'acupuncture ne s'est pas construite sur le savoir scientifique et ses fondements sont considérés
-                    comme relevant de la pseudo-science. Certains travaux de chercheurs tendent à montrer que
-                    l’acuponcture n’a pas d’efficacité propre, mais pourrait agir sur le patient par effet placebo
-                    entrainant des mécanismes neuropsychologiques, ou neurophysiologiques réels, indiquant une
-                    efficacité notamment dans le traitement de la douleur. Le risque d'iatrogénie existe, notamment par
-                    la transmission d'agents pathogènes si les conditions d'asepsie sont insuffisantes. L'utilisation de
-                    l'acupuncture a été critiquée sur le plan éthique lorsqu'elle se fait au détriment de traitements
-                    efficaces.
-                    La pratique de l'acupuncture est attestée depuis plusieurs millénaires en Asie (en particulier en
-                    Inde, Chine, Corée et Japon), même si son succès a beaucoup fluctué avec le temps. De par ses
-                    racines historiques, géographiques et culturelles, de ses conditions d’implantation en Occident,
-                    l’acupuncture n'intéresse pas seulement la médecine mais aussi l'anthropologie, l'histoire des
-                    sciences, l'épistémologie, la sociologie et ce depuis son introduction en Europe au xviie siècle.
-                    L'UNESCO l'a inscrite au patrimoine culturel immatériel de l'humanité le 16 novembre 2010.</p>
-            </div>
-
-            <div class=col-sm>
-                <img class=imgdesc src="views/imagesindex/img_acuponcture1.jpg"> <br>
-                <img class=imgdesc2 src="views/imagesindex/img_acuponcture2.jpg">
-            </div>
-
-
+    <div class=row>
+        <div class="col mx-1">
+            <h2>Qu'est-ce que l'acuponcture ?</h2>
+            <p>L’acupuncture ou acuponcture est un système thérapeutique dont les origines historiques sont
+                très liées avec la tradition médicale chinoise. L'acupuncture consiste en une stimulation de zones
+                précises de l'épiderme : les « points d’acupuncture ». Les techniques de stimulation des points
+                d’acupuncture sont effectuées avec des moyens divers : des aiguilles le plus souvent, mais aussi
+                d'autres moyens physiques (mécaniques, électriques, magnétiques, thermiques, lumineux) ou
+                physico-chimiques, voire d'autres méthodes alternatives dangereuses (apipuncture, apithérapie avec
+                des piqûres d'abeille).
+                L'acupuncture ne s'est pas construite sur le savoir scientifique et ses fondements sont considérés
+                comme relevant de la pseudo-science. Certains travaux de chercheurs tendent à montrer que
+                l’acuponcture n’a pas d’efficacité propre, mais pourrait agir sur le patient par effet placebo
+                entrainant des mécanismes neuropsychologiques, ou neurophysiologiques réels, indiquant une
+                efficacité notamment dans le traitement de la douleur. Le risque d'iatrogénie existe, notamment par
+                la transmission d'agents pathogènes si les conditions d'asepsie sont insuffisantes. L'utilisation de
+                l'acupuncture a été critiquée sur le plan éthique lorsqu'elle se fait au détriment de traitements
+                efficaces.
+                La pratique de l'acupuncture est attestée depuis plusieurs millénaires en Asie (en particulier en
+                Inde, Chine, Corée et Japon), même si son succès a beaucoup fluctué avec le temps. De par ses
+                racines historiques, géographiques et culturelles, de ses conditions d’implantation en Occident,
+                l’acupuncture n'intéresse pas seulement la médecine mais aussi l'anthropologie, l'histoire des
+                sciences, l'épistémologie, la sociologie et ce depuis son introduction en Europe au xviie siècle.
+                L'UNESCO l'a inscrite au patrimoine culturel immatériel de l'humanité le 16 novembre 2010.</p>
         </div>
-
+        <div class="col mx-1">
+            <img class=imgdesc src="views/imagesindex/img_acuponcture1.jpg">
+            <img class=imgdesc2 src="views/imagesindex/img_acuponcture2.jpg">
+        </div>
     </div>
 </section>
 
 <section id="Les_bienfaits">
     <!-- Fond plein avec icon + compliment et raison de faire des scéances chez nous -->
     <div class="row">
-        <div class="col-sm-12">
-            <h1 class=Page>Avis de nos clients</h1>
-            <div class="row">
-                <div class="col-8 col-sm-6 avis">
-                    <p class=avis><br>Mon experience avec l'acupuncture a été formidable. Je souffrais de migraines
-                        ophtalmiques depuis une
-                        bonne quinzaine d'années et cela devenait insupportable. Il m'était arrivée de souffrir pendant
-                        trois
-                        semaines d'affilées. J'avais essayé différents traitements et rien ne pouvait stopper ces
-                        douleurs. Je ne dormais plus et je travaillais en école maternelle ! Ma directrice me recommanda
-                        ce docteur qui
-                        faisait
-                        des merveilles. Je ne croyais plus en rien mais j'eu tout de même recours à lui et
-                        "merveille"!... il sut
-                        placer ses aiguilles et me débarassa de ce fléau !</p>
-                </div>
-                <div class="col-4 col-sm-6 avis">
-                    <p class=avis><br>Cela fait des années que je vois une homéopathe acupuncteur. J'apprécie les
+        <div class="col mx-1 text-center">
+            <h2>Avis de nos clients</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6">
+            <figure class="text-center">
+                <blockquote class="blockquote">
+                    <p class="mb-0 fs-6">Mon experience avec l'acupuncture a été formidable. Je souffrais de migraines
+                    ophtalmiques depuis une
+                    bonne quinzaine d'années et cela devenait insupportable. Il m'était arrivée de souffrir pendant
+                    trois
+                    semaines d'affilées. J'avais essayé différents traitements et rien ne pouvait stopper ces
+                    douleurs. Je ne dormais plus et je travaillais en école maternelle ! Ma directrice me recommanda
+                    ce docteur qui
+                    faisait
+                    des merveilles. Je ne croyais plus en rien mais j'eu tout de même recours à lui et
+                    "merveille"!... il sut
+                    placer ses aiguilles et me débarassa de ce fléau !</p>
+                
+                </blockquote>
+                <figcaption class="blockquote-footer">Louis XIV</figcaption>
+            </figure>
+        </div>
+        <div class="col-md-6">
+            <figure class="text-center">
+                <blockquote class="blockquote">
+                    <p class="mb-0 fs-6">Cela fait des années que je vois une homéopathe acupuncteur. J'apprécie les
                         médecines douces, un
                         traitement de fond en homéopathie et des séances 2 ou 3 fois par an d'acupuncture qui vient en
                         complément et qui permet une meilleure circulation de l'énergie. J'ai la certitude que ces 2
@@ -103,22 +106,12 @@ $_smarty_tpl->_subTemplateRender("file:../templates/structure.html", $_smarty_tp
                         (adaptées à la personnalité de chacun) m'aide à mieux vivre au quotidien, à avoir une meilleure
                         santé et
                         je les conseille donc sans réserve. M.Line</p>
-                </div>
-            </div>
-            <br><br><br><br>
+                </blockquote>
+                <figcaption class="blockquote-footer">Tom Tonkin</figcaption>
+            </figure>
         </div>
     </div>
 </section>
-<section id="Nous trouver">
-    <!-- Coordonée plus carte section divisé horizontalement -->
-    <div class="row">
-        <div class="col">
-
-        </div>
-    </div>
-</section>
-
-</body>
-
-</html><?php }
+<?php $_smarty_tpl->_subTemplateRender("file:../templates/structure-f.html", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
 }
